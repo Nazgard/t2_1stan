94f531fcfa5f8399bb7a6ad00fbbe19389c0a36e
Nazgard <nazgardo@ya.ru>
2014-03-03 15:31:49 +0600

    prepare for COM

--- t2_1stan_imitation/MainWindow.xaml.cs
+++ t2_1stan_imitation/MainWindow.xaml.cs
@@ -23,7 +23,7 @@ namespace t2_1stan_imitation
     /// </summary>
     public partial class MainWindow : Window
     {
-        private int px_meter_factor = 30;
+        private double px_meter_factor = 30;
         private byte segments_tube = 0x00;
         private byte current_segment_tube = 0x00;
         private bool position_defectoscope = false;
@@ -31,12 +31,15 @@ namespace t2_1stan_imitation
         private System.Windows.Threading.DispatcherTimer move_tubeTimer = new System.Windows.Threading.DispatcherTimer();
         private DoubleAnimation animation1 = new DoubleAnimation();
         private SerialPort serialPort = new SerialPort();
+        private Crc8 crc8 = new Crc8();
 
         public MainWindow()
         {
             InitializeComponent();
             try
             {
+                serialPort.PortName = "COM3";
+                serialPort.Open(); 
                 position_stop = Canvas.GetLeft(rectangle5) + rectangle5.Width;
                 move_tubeTimer.Tick += new EventHandler(move_tube);
                 move_tubeTimer.Interval = TimeSpan.FromMilliseconds(500);
@@ -66,7 +69,9 @@ namespace t2_1stan_imitation
         private void button3_Click(object sender, RoutedEventArgs e)
         {
             try
-            {                
+            {
+                textBox1.IsEnabled = false;
+                button2.IsEnabled  = false;
                 move_tubeTimer.Start();
             }
             catch (Exception ex)
@@ -84,6 +89,7 @@ namespace t2_1stan_imitation
             animation1.To = -left;
             animation1.Duration = TimeSpan.FromMilliseconds(500);
             rectangle_tube.BeginAnimation(Canvas.LeftProperty, animation1);
+            PacOut3();
         }
 
         private void move_tube(object sender, EventArgs e)
@@ -99,12 +105,11 @@ namespace t2_1stan_imitation
 
                     if (Canvas.GetLeft(rectangle5) <= (Canvas.GetLeft(rectangle_tube) + rectangle_tube.Width) && Canvas.GetLeft(rectangle5) >= Canvas.GetLeft(rectangle_tube))
                     {
+                        PacOut2(current_segment_tube, 0);
                         current_segment_tube++;
                     }
 
                     Label1.Content = current_segment_tube.ToString();
-
-                    PacOut2(current_segment_tube);
                 }                    
                 else
                 {
@@ -119,6 +124,8 @@ namespace t2_1stan_imitation
 
         private void button1_Click(object sender, RoutedEventArgs e)
         {
+            textBox1.IsEnabled = true;
+            button2.IsEnabled = true;
             move_tubeTimer.Stop();
         }
 
@@ -136,9 +143,6 @@ namespace t2_1stan_imitation
                 this.Left = ps.Left;
 
                 reset_rectangle_tube_width();
-
-                serialPort.PortName = "COM3";
-                serialPort.Open(); 
             }
             catch (Exception ex)
             {
@@ -186,22 +190,6 @@ namespace t2_1stan_imitation
             }
         }
 
-        private static string CheckHex(string input)
-        {
-            string result = input;
-            if (input.Length % 2 != 0)
-                result = '0' + result;
-            return result;
-        }
-
-        private static string Dec2Hex(byte input)
-        {
-            string result = string.Empty;
-            result = input.ToString("X");
-            return CheckHex(result);
-        }
-
-
         // ======================================================
         // 30 импульсов на метр.
         // 5 импульсов = 1 сегмент
@@ -219,7 +207,7 @@ namespace t2_1stan_imitation
         */
         private void PacOut1(byte deffect)
         {
-            byte[] Packets = new byte[10];
+            byte[] Packets = new byte[11];
 
             Packets[0]  = 0xE6;
             Packets[1]  = 0x19;
@@ -227,15 +215,15 @@ namespace t2_1stan_imitation
             Packets[3]  = 0x08;
 
             Packets[4]  = 0x01;
-            Packets[5] =  deffect;
+            Packets[5]  = deffect;
             Packets[6]  = 0x00;
-            Packets[7]  = 0x00;
+            Packets[7]  = crc8.ComputeChecksum(Packets, 11 - 4);
 
             Packets[8]  = 0x00;
             Packets[9]  = 0x00;
             Packets[10] = 0x00;
 
-            serialPort.Write(Packets, 0, Packets.Length-1);
+            serialPort.Write(Packets, 0, Packets.Length);
         }
 
         /*
@@ -247,7 +235,7 @@ namespace t2_1stan_imitation
         *    0xCRC          контрольная сумма
         *    0x00 0x00 0x00 окончание пакета
         */
-        private void PacOut2(byte NN)
+        private void PacOut2(byte NN, byte deffect)
         {
             byte[] Packets = new byte[11];
 
@@ -258,14 +246,14 @@ namespace t2_1stan_imitation
 
             Packets[4]  = 0x02;
             Packets[5]  = NN;
-            Packets[6]  = 0x00;
-            Packets[7]  = 0x00;
+            Packets[6]  = deffect;
+            Packets[7]  = crc8.ComputeChecksum(Packets, 11-4);
 
             Packets[8]  = 0x00;
             Packets[9]  = 0x00;
             Packets[10] = 0x00;
 
-            serialPort.Write(Packets, 0, Packets.Length - 1);
+            serialPort.Write(Packets, 0, Packets.Length);
         }
 
         /*
@@ -279,7 +267,7 @@ namespace t2_1stan_imitation
         */
         private void PacOut3()
         {
-            byte[] Packets = new byte[10];
+            byte[] Packets = new byte[11];
 
             Packets[0]  = 0xE6;
             Packets[1]  = 0x19;
@@ -289,13 +277,13 @@ namespace t2_1stan_imitation
             Packets[4]  = 0x03;
             Packets[5]  = segments_tube;
             Packets[6]  = 0x00;
-            Packets[7]  = 0x00;
+            Packets[7]  = crc8.ComputeChecksum(Packets, 11-4);
 
             Packets[8]  = 0x00;
             Packets[9]  = 0x00;
             Packets[10] = 0x00;
 
-            serialPort.Write(Packets, 0, Packets.Length - 1);
+            serialPort.Write(Packets, 0, Packets.Length);
         }
     }
 }
